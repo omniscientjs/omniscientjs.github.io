c89516f8e4981caf6ffeeee4377ea658b4ede995
Mikael Brevik <mikaelbre@gmail.com>
2014-10-10 22:02:20 +0200

    Represent index.html as React app instead

--- browser/main.js
+++ browser/main.js
@@ -2,12 +2,32 @@ var fs = require('fs');
 var marked = require('marked');
 var highlight = require('highlight.js');
 
-// Synchronous highlighting with highlight.js
+var component = require('omniscient');
+var immstruct = require('immstruct');
+var React = require('react');
+
 marked.setOptions({
   highlight: function (code) {
     return highlight.highlightAuto(code).value;
   }
 });
 
-var markdown = fs.readFileSync(__dirname + '/../index.md', 'utf-8');
-document.querySelector('.content-container').innerHTML = marked(markdown);
+var structure = immstruct({
+  index: fs.readFileSync(__dirname + '/../index.md', 'utf-8')
+});
+
+var App = component(function (cursor) {
+  return React.DOM.div({
+    dangerouslySetInnerHTML: {
+      __html:marked(cursor.get('index'))
+    }
+  });
+});
+
+var container = document.querySelector('.content-container');
+var render = function () {
+  React.renderComponent(App(structure.cursor()), container);
+}
+
+render();
+structure.on('swap', render);

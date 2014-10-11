934bc5c931564fe95a9f8e86eb8a86254f0d29b3
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-10-11 15:44:26 +0200

    Whitespace.

--- browser/lib/loadContentToCursor.js
+++ browser/lib/loadContentToCursor.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 module.exports = function (doc, cursor) {
   var url = '/documents/' + doc + '.md';
-  return http.get({ path : url }, function (response) {
+  return http.get({ path : url }, function (response) {
     if (response.statusCode !== 200) {
       return cursor.set(doc, '## Could not find the document "' + doc + '"');
     }

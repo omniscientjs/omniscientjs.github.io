9cb5b03c35a1345b34215ed47f3802b53ab9b051
Mikael Brevik <mikaelbre@gmail.com>
2014-10-13 12:53:42 +0200

    Cleans up entry-list example

--- examples/entry-list/list.js
+++ examples/entry-list/list.js
@@ -3,21 +3,25 @@ var EventEmitter = require("events").EventEmitter,
     component = require('omniscient');
 
 var Item = require('./item');
+
 var events = new EventEmitter();
 
-module.exports = component(function (cursor, statics) {
-  console.log("rerender");
+var didMountMixin = {
+  componentDidMount: function () {
+    var self = this;
+    // Will rerender, so remove all listeners.
+    events.on('delete', function (item) {
+      self.props.cursor.update(function (state) {
+        // Use toVector() https://github.com/facebook/immutable-js/issues/122
+        return state.splice(state.indexOf(item), 1).toVector();
+      });
+    });
+  }
+};
+
+module.exports = component(didMountMixin, function (cursor, statics) {
   var items = cursor.toArray().map(function (item, key) {
     return Item("item-" + key, item, { events: events });
-  });
-
-  // Will rerender, so remove all listeners.
-  events.removeAllListeners();
-  events.once('delete', function (item) {
-    cursor = cursor.update(function (state) {
-      // Use toVector() https://github.com/facebook/immutable-js/issues/122
-      return state.splice(state.indexOf(item), 1).toVector();
-    });
   });
 
   return React.DOM.section({},

ba94ead42702b4c0e0352fbea9bd4824f0563144
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-02-15 20:47:11 +0100

    Playground!

--- scripts/runnable-scripts.js
+++ scripts/runnable-scripts.js
@@ -1,11 +1,11 @@
 (function () {
 
-  $script(['scripts/vendor/codemirror-min.js',
+  $script(['/scripts/vendor/codemirror-min.js',
           '//cdnjs.cloudflare.com/ajax/libs/immutable/3.6.2/immutable.min.js',
           '//cdnjs.cloudflare.com/ajax/libs/react/0.12.2/react.min.js'], function () {
     $script(['//cdnjs.cloudflare.com/ajax/libs/immstruct/1.4.0/immstruct.min.js',
             '//cdnjs.cloudflare.com/ajax/libs/omniscient/2.1.0/omniscient.min.js',
-            'scripts/vendor/browser.js'], runnableScriptsLoad);
+            '/scripts/vendor/browser.js'], runnableScriptsLoad);
   });
 
   function runnableScriptsLoad () {
@@ -17,30 +17,49 @@
   }
 
   function createEditors (runnable) {
-    var editor = CodeMirror.fromTextArea(runnable, {
+    var useHash = runnable.dataset['hash'];
+    var options = {
       lineWrapping: true,
       viewportMargin: Infinity,
-      theme: 'kimbie-dark'
-    });
+      theme: 'kimbie-dark',
+      tabSize: 2,
+      extraKeys: {
+        Tab: betterTab
+      }
+    };
+
+    var editor = CodeMirror.fromTextArea(runnable, options);
+    var src = editor.doc.getValue();
+
+    var urlCode;
+
+    if (useHash) {
+      var initialCode = location.hash.replace(/^#/, '');
+      src = decodeURIComponent(initialCode) || src;
+      editor.setValue(src);
+    }
+
     var timers = { timeouts: [], intervals: [] };
     editor.on('change', function () {
-      run(editor, timers);
+      run(editor.doc.getValue(), timers);
     });
-    run(editor, timers);
+    run(src, timers);
   }
 
-  function run (editor, timers) {
-    var src = editor.doc.getValue();
-    var match = src.match(/['"](editor-[0-9]+)['"]/);
+  function run (src, timers) {
+    var match = src.match(/['"](result(-[0-9]+)?)['"]/);
     var resultEl;
     if (match && match[1]) {
       resultEl = document.getElementById(match[1]);
     }
     try {
+
       timers.timeouts.forEach(clearTimeout);
       timers.intervals.forEach(clearInterval);
+
       timers.timeouts = [];
       timers.intervals = [];
+
       var newSetTimeout = function () {
         var id = setTimeout.apply(this, arguments);
         timers.timeouts.push(id);
@@ -51,25 +70,40 @@
         timers.intervals.push(id);
         return id;
       };
+
       var compiledCode = to5.transform(src).code;
-      var fn = new Function('setTimeout', 'setInterval', compiledCode);
-      fn(newSetTimeout, newSetInterval);
+      var fn = Function.apply(null, [
+        'setTimeout',
+        'setInterval',
+        compiledCode]);
+
+      fn(newSetTimeout,
+         newSetInterval);
     }
     catch (e) {
       var msg = e.message;
       if (resultEl) {
-        resultEl.innerHTML = '';
-        var errorEl = document.createElement('span');
-        errorEl.className = 'editor-error';
-        // var error = document.createTextNode(msg)
         var error = document.createElement('pre');
         error.innerText = msg;
+
+        var errorEl = document.createElement('span');
+        errorEl.className = 'editor-error';
         errorEl.appendChild(error);
+
+        resultEl.innerHTML = '';
         resultEl.appendChild(errorEl);
       }
       else {
         console.error(msg);
       }
+    }
+  }
+
+  function betterTab(cm) {
+    if (cm.somethingSelected()) {
+      cm.indentSelection("add");
+    } else {
+      cm.execCommand('insertSoftTab')
     }
   }
 })();

12f43ac2f13e16a3f40aceb128a375c0c97c587a
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-02-22 19:06:11 +0100

    Indents.

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,28 @@ var prefix      = require('gulp-autoprefixer');
 var cp          = require('child_process');
 
 gulp.task('jekyll-build', function (done) {
-    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
-        .on('close', done);
+  return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+  .on('close', done);
 });
 
 gulp.task('sass', function () {
-    return gulp.src('_sass/main.scss')
-        .pipe(sass({
-            includePaths: ['_sass']
-        }))
-        .on('error', console.error.bind(console))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7']))
-        .pipe(gulp.dest('_site/css'))
-        .pipe(gulp.dest('css'));
+  return gulp.src('_sass/main.scss')
+  .pipe(sass({
+    includePaths: ['_sass']
+  }))
+  .on('error', console.error.bind(console))
+  .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7']))
+  .pipe(gulp.dest('_site/css'))
+  .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', ['default'], function () {
-    gulp.watch('_sass/**/*.scss', ['sass']);
-    gulp.watch([
-      'scripts/**/*.js',
-      '**/*.html',
-      '!_site/**/*',
-       '**/*.md'], ['jekyll-build']);
+  gulp.watch('_sass/**/*.scss', ['sass']);
+  gulp.watch([
+    'scripts/**/*.js',
+    '**/*.html',
+    '!_site/**/*',
+    '**/*.md'], ['jekyll-build']);
 });
 
 gulp.task('default', ['sass', 'jekyll-build']);

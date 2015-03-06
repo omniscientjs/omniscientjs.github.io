77cce6670c85b8e0b09b5e5d725da14cbb7831b5
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-03-06 22:46:30 +0100

    Adds task immstruct clock

---
+++ _workshop/32-immstruct-clock.js
@@ -0,0 +1,26 @@
+---
+layout: workshop
+collection: workshop
+title: Immstruct Clock
+section: 3
+name: 32-immstruct-clock
+next: 33-
+slides: http://omniscientjs.github.io/workshop-talk
+---
+
+var data = immstruct({ time: new Date() });
+
+var Clock = React.createClass({
+  render: function () {
+    var time = this.props.time.deref().toString()
+    return <span>{time}</span>;
+  }
+});
+
+var render = () => React.render(<Clock time={data.cursor('time')} />, el);
+data.on('swap', () => render());
+render();
+
+setInterval(() => {
+  data.cursor('time').update(_ => new Date());
+}, 1000);

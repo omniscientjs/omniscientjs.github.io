7793db6865ad7b2c426488c0a3115eb258a2eb5a
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-03-06 22:46:10 +0100

    Adds task immstruct counter

---
+++ _workshop/31-immstruct-counter.js
@@ -0,0 +1,25 @@
+---
+layout: workshop
+collection: workshop
+title: Immstruct Counter
+section: 3
+name: 31-immstruct-counter
+next: 32-
+slides: http://omniscientjs.github.io/workshop-talk
+---
+
+var Counter  = React.createClass({
+
+  render: function () {
+    var counter = this.props.counter;
+    var inc = (e) => counter.update(v => v + 1);
+    return <button onClick={inc}>{counter.deref()}</button>;
+  }
+});
+
+
+var data = immstruct({ counter: 0 });
+
+var render = () => React.render(<Counter counter={data.cursor('counter')} />, el);
+data.on('swap', () => render());
+render();

2c7b6574ef494d8763ce3f31ca9a1947a8bb14c0
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-03-09 21:53:21 +0100

    Remove solution for 45

--- _workshop/45-add-mixins-and-lifecycle-mixins.js
+++ _workshop/45-add-mixins-and-lifecycle-mixins.js
@@ -14,47 +14,38 @@ var component = omniscient.withDefaults({ jsx: true });
 var focusOnRenderMixin = {
   // Create the componentDidMount life cycle method and
   // select the current dom node to put the mouse cursor inside it
-  componentDidMount: function () {
-    this.getDOMNode().select();
-  }
+
 };
 
 // Create a FocusingInput component that makes use of the focusOnRenderMixin
-var FocusingInput = component(focusOnRenderMixin, function ({inputCursor}) {
+var FocusingInput = component(function () {
 
   // Create an onChange function that will set the 'text' of the inputCursor
   // to the value of the input from the event
-  var onChange = (e) => {
-    inputCursor.set('text', e.currentTarget.value);
-  };
 
   // Create an input element that listens for change as you type
   // and that shows the 'text' of the inputCursor
-  return <input onChange={onChange} value={inputCursor.get('text')} />;
+
 });
 
 var App = component(({inputCursor}) => (
   <div>
+    <h1>Focusing input</h1>
     // show the 'text' of the inputCursor
-    <h1>Input: {inputCursor.get('text')}</h1>
 
     // render a FocusingInput passing the inputCursor as its prop
-    <FocusingInput inputCursor={inputCursor} />
+
   </div>
 ));
 
 
 // Create a immutable data structure
-var data = immstruct({
-  text: 'Gambit'
-});
 
 // Render every time immutable data swaps.
-data.on('swap', render);
 
 // Initial render
 render();
 
 function render () {
-  React.render(<App inputCursor={data.cursor()} />, el);
+  React.render(<App />, el);
 }

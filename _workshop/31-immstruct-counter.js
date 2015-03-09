2d558b35f63b9a5d560cd7506fc5b68b683d7ecb
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-03-09 22:24:05 +0100

    Remove solution for 31

--- _workshop/31-immstruct-counter.js
+++ _workshop/31-immstruct-counter.js
@@ -14,23 +14,14 @@ slides: http://omniscientjs.github.io/workshop-slides/#57
 // The component should render a button that when clicked
 // will increate the value of the `counter` cursor it is passed
 // This should make the component rerender
-var Counter  = React.createClass({
-
-  render: function () {
-    var counter = this.props.counter;
-    var inc = (e) => counter.update(v => v + 1);
-    return <button onClick={inc}>{counter.deref()}</button>;
-  }
-});
-
+var Counter;
 
 var data = immstruct({ counter: 0 });
 
 // Render the Counter component with a prop `counter` with a cursor
 // of the `counter` property in data
-var render = () => React.render(<Counter counter={data.cursor('counter')} />, el);
+var render = () => React.render(<div />, el);
 
 render();
 
 // When data changes, rerender
-data.on('swap', render);

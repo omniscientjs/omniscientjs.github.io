2606784fd82784e89aef61757554dfef364fbea0
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-03-09 22:23:57 +0100

    Remove solution for 32

--- _workshop/32-immstruct-clock.js
+++ _workshop/32-immstruct-clock.js
@@ -14,23 +14,18 @@ var data = immstruct({ time: new Date() });
 // Create a Clock component that deref()'s its time prop
 // that holds a cursor to the `time` of `data`
 // Remeber to call .toString() on the time to stringify it
-var Clock = React.createClass({
-  render: function () {
-    var time = this.props.time.deref().toString()
-    return <span>{time}</span>;
-  }
-});
+var Clock;
 
 // Create the render function. It should render the Clock component
 // passing a prop `time` with a cursor to `time` of the `data` structure
-var render = () => React.render(<Clock time={data.cursor('time')} />, el);
+var render = () => React.render(<div />, el);
+
 render();
 
 // At an interval update the `time` of data with a new value,
 // so the clock will tick every second
 setInterval(() => {
-  data.cursor('time').update(_ => new Date());
+
 }, 1000);
 
 // Rerender when data changes
-data.on('swap', render);
